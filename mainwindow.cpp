6f53f748dbf8d7bbb785b9ca07fe6c41d8f6ef86
Tich <lewis.smith.tian@gmail.com>
2017-08-21 21:02:42 +0800

    create

---
+++ mainwindow.cpp
@@ -0,0 +1,185 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    manager = new QNetworkAccessManager(this);
+    connect(manager, SIGNAL(finished(QNetworkReply*)),
+            this, SLOT(replyFinished(QNetworkReply*)));
+    first = true;
+//    manager->get(QNetworkRequest(QUrl("http://music.163.com/api/song/lyric?os=pc&id=488256319&lv=-1&kv=-1&tv=-1")));
+}
+
+MainWindow::~MainWindow(){
+    delete ui;
+}
+
+void MainWindow::replyFinished(QNetworkReply *reply){
+    if (reply->error() == QNetworkReply::NoError){
+        QByteArray bytes = reply->readAll();
+        QString string = QString::fromUtf8(bytes);
+        Get_Data(string);
+    } else {
+        first = true;
+        ui->Lrc->setText("handle errors here");
+//        QVariant statusCodeV = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+//        //statusCodeV是HTTP服务器的相应码，reply->error()是Qt定义的错误码，可以参考QT的文档
+//        qDebug( "found error ....code: %d %d\n", statusCodeV.toInt(), (int)reply->error());
+//        qDebug(qPrintable(reply->errorString()));
+    }
+    if(!first){
+        QString s = QString("http://music.163.com/api/song/lyric?os=pc&id=%1&lv=-1&kv=-1&tv=-1").arg(songID);
+//        qDebug()<<s;
+        set_get_request(s);
+    }
+    reply->deleteLater();
+}
+
+void MainWindow::Get_Data(QString string){
+    if(first){
+        QRegExp rx;
+        rx.setMinimal(true);
+        rx.setPattern("data-res-name=\"(.*)\"");
+        int pos = string.indexOf(rx);
+        if ( pos >= 0 ){
+            ui->songNameEdit->setText(rx.cap(1));
+        }
+        rx.setPattern("data-res-author=\"(.*)\"");
+        pos = string.indexOf(rx);
+        if ( pos >= 0 ){
+            ui->singerEdit->setText(rx.cap(1));
+        }
+        first = false;
+    }
+    else{
+        first = true;
+        //QString => QJsonDocument
+        QJsonDocument jsonDocument = QJsonDocument::fromJson(string.toUtf8().data());
+        if(jsonDocument.isNull()){
+            qDebug()<< "===> please check the string "<< string.toUtf8().data();
+            return;
+        }
+        QJsonObject jsonObject = jsonDocument.object();
+        // get the response object
+        QJsonValue lrc = jsonObject.value("lrc");
+        QJsonObject lrcObj = lrc.toObject();
+        QJsonValue lyric = lrcObj.value("lyric");
+//        qDebug()<<lyric;
+        if(lyric.isUndefined()){
+            ui->Lrc->setText("无歌词...");
+            return;
+        }
+        QRegExp rx("\\[\\d.*\\]");
+        rx.setMinimal(true);
+        raw_ = raw = lyric.toString();
+        ui->Lrc->setText(raw);
+        raw_.replace(rx,"");
+        //翻译
+        if(jsonObject.find("tlyric") != jsonObject.end()){
+            lrc = jsonObject.value("tlyric");
+            lrcObj = lrc.toObject();
+            lyric = lrcObj.value("lyric");
+//            qDebug()<<lyric;
+            if(lyric.isNull()){
+                trans_ = trans = "";
+                return;
+            }
+            trans_ = trans = lyric.toString();
+            ui->Lrc->setText(raw + trans);
+            trans_.replace(rx, "");
+        }
+    }
+}
+
+
+void MainWindow::set_get_request(QString url){
+    QNetworkRequest *request = new QNetworkRequest();
+    request->setUrl(QUrl(url));
+    request->setRawHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36");
+    manager->get(*request);
+}
+
+
+void MainWindow::on_pushButton_clicked(){
+    //GET
+    if(ui->songUrlEdit->text().isEmpty()){
+        return;
+    }
+    QRegExp rx("(\\d+\\d+\\d+\\d+)");
+    int pos = ui->songUrlEdit->text().indexOf(rx);
+    if ( pos >= 0 ){
+        songID = rx.cap(1);
+//        qDebug()<<songID;
+        QString s = QString("http://music.163.com/song?id=%1").arg(songID);
+        set_get_request(s);
+    }
+}
+
+//键盘事件 ctrl+s
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if (event->modifiers() == Qt::ControlModifier){
+        if(event->key() == Qt::Key_S){
+            path = QFileDialog::getSaveFileName(this, "Sava File", "", "*.lrc");
+            if(path.isEmpty()){
+                return;
+            }
+            QFile outFile(path);
+            if (!outFile.open(QIODevice::WriteOnly|QIODevice::Text)) {
+                QMessageBox::critical(NULL, "提示", "无法创建文件");
+                return;
+            }
+            QTextStream outTS(&outFile);
+            outTS.setCodec(QTextCodec::codecForName("UTF-8"));
+            outTS<<ui->Lrc->toPlainText().toUtf8();
+            outTS.setGenerateByteOrderMark(true);
+            outTS.flush();
+            outFile.close();
+        }
+
+    }
+}
+
+void MainWindow::on_timeCheck_clicked(){
+    if(raw.isEmpty()){
+        return;
+    }
+    if(ui->timeCheck->isChecked()){
+        if(ui->transCheck->isChecked()){
+            ui->Lrc->setText(raw + trans);
+        }else{
+            ui->Lrc->setText(raw);
+        }
+    }else{
+        if(ui->transCheck->isChecked()){
+            ui->Lrc->setText(raw_ + trans_);
+        }else{
+            ui->Lrc->setText(raw_);
+        }
+    }
+
+}
+
+void MainWindow::on_transCheck_clicked(){
+    if(trans.isEmpty()){
+        return;
+    }
+    if(ui->timeCheck->isChecked()){
+        if(ui->transCheck->isChecked()){
+            ui->Lrc->setText(raw + trans);
+        }else{
+            ui->Lrc->setText(raw);
+        }
+    }else{
+        if(ui->transCheck->isChecked()){
+            ui->Lrc->setText(raw_ + trans_);
+        }else{
+            ui->Lrc->setText(raw_);
+        }
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,54 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QUrl>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QFileDialog>
+#include <QKeyEvent>
+#include <QMessageBox>
+#include <QTextCodec>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void replyFinished(QNetworkReply *reply);
+    void keyPressEvent(QKeyEvent *event);
+
+    void on_pushButton_clicked();
+
+
+    void on_timeCheck_clicked();
+
+    void on_transCheck_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QNetworkAccessManager *manager;
+    QString path;
+    void set_get_request(QString url);
+    void Get_Data(QString string);
+    bool first;
+    QString raw;//原歌词
+    QString raw_;//无时间轴
+    QString trans;//翻译
+    QString trans_;//无时间轴
+    QString songID;//歌曲id
+
+};
+
+#endif // MAINWINDOW_H
